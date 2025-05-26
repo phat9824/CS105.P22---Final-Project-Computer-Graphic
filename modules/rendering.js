31d8b208985895fef6b6715861fdadf65822216c
nguyenquan132 <quannguyendinh.221204@gmail.com>
2025-05-27 01:31:42 +0700

    Added painting information for image

--- modules/rendering.js
+++ modules/rendering.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { displayPaintingInfo, hidePaintingInfo } from './paintingInfo';
 
 
 export function setupRendering(scene, camera, renderer, paintings, controls) {
@@ -8,6 +9,22 @@ export function setupRendering(scene, camera, renderer, paintings, controls) {
     const delta = clock.getDelta();
     if (typeof window.updateMovement === 'function') {
       window.updateMovement(delta);
+    }
+
+    const distanceThreshold = 8; // set a distance threshold (8 units)
+    let paintingToShow;
+    paintings.forEach((painting) => {
+      const distanceToPainting = camera.position.distanceTo(painting.postion) // get distacne to painting 
+      if (distanceToPainting < distanceThreshold) {
+        paintingToShow = painting; // set paintingToShow to this painting (painting will show)
+      }
+    });
+
+    if (paintingToShow) {
+      // if there is a painting to show
+      displayPaintingInfo(paintingToShow.userData.info);
+    } else {
+      hidePaintingInfo();
     }
 
     renderer.render(scene, camera);

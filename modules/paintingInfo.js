686a1caf6455d293a7a2a5568fa32422bc1508ed
nguyenquan132 <quannguyendinh.221204@gmail.com>
2025-05-27 01:30:06 +0700

    Added painting infomation module

---
+++ modules/paintingInfo.js
@@ -0,0 +1,18 @@
+// Display painting info in the DOM 
+export const displayPaintingInfo = (info) => {
+    const infoElement = document.getElementById('painting-info');
+
+    infoElement.innerHTML = `
+        <h3>${info.title}</h3>
+        <p>Artist: ${info.artist}</p>
+        <p>Description: ${info.description}</p>
+        <p>Year: ${info.year}</p>
+    `;
+    infoElement.classList.add('show'); // Add the 'show' class
+}
+
+// Hide painting info in the DOM 
+export const hidePaintingInfo = () => {
+    const infoElement = document.getElementById('painting-info');
+    infoElement.classList.remove('show'); // Remove the 'show' class 
+}
